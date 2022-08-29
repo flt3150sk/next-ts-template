2f3eb512f1c16153b08f0864faf2fb8fded80574
flt3150sk <muranaka.kouske@gmail.com>
2022-08-29 10:45:54 +0900

    fix: button-test

--- src/components/commons/Button/index.test.tsx
+++ src/components/commons/Button/index.test.tsx
@@ -1,16 +1,9 @@
 import { composeStories } from '@storybook/testing-react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import * as stories from './index.stories';
 
 const { Primary } = composeStories(stories);
-
-test('ボタンが表示されること', () => {
-  render(<Primary label="Button" />);
-  const button = screen.getByText(/Button/i);
-
-  expect(button).not.toBeNull();
-});
 
 test('ボタンのクリックイベントが正しく発火すること', async () => {
   const spyFn = jest.fn();
